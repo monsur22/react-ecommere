4d60c514b2e1a1e3e2fb130a90b93fa8c3edb93e
monsur22 <ahmed-15-5439@diu.edu.bd>
2021-06-16 21:22:46 +0600

    Two layout or dashboard in one project

---
+++ src/components/admin/AdminNav.js
@@ -0,0 +1,18 @@
+import React, { Component }  from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+class AdminNav extends Component {
+  render() {
+    return(
+
+      <h1>Admin Nav</h1>
+
+
+    );
+  }
+}
+export default AdminNav;
---
+++ src/components/home/Nav.js
@@ -0,0 +1,18 @@
+import React, { Component }  from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+class Nav extends Component {
+  render() {
+    return(
+
+      <h1>Home Nav</h1>
+
+
+    );
+  }
+}
+export default Nav;
