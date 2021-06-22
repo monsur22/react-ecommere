de9dc4c3589b9c04e19936b6f0502e103aaa0694
monsur22 <ahmed-15-5439@diu.edu.bd>
2021-06-22 22:32:08 +0600

    desing update

--- src/App.js
+++ src/App.js
@@ -8,18 +8,22 @@ import {
 } from "react-router-dom";
 
 import Home from './components/home/Home';
-import AdminHome from './components/admin/AdminHome';
+import Nav from './components/home/Nav';
+import Footer from './components/home/Footer';
+// import AdminHome from './components/admin/AdminHome';
 
 function App() {
   return (
     <Router>
     <div>
+    <Nav />
         <Switch>
           <Route  exact  path="/"><Home /></Route>
-          <Route    path="/Admin"><AdminHome /></Route>
+          {/* <Route    path="/Admin"><AdminHome /></Route> */}
           {/* <Route path="/services"><Services /></Route> */}
 
         </Switch>
+    <Footer/>
     </div>
   </Router>
   );
