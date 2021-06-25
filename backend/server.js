931d88b04c2b54722f2a07c11d444f8c59e65649
monsur22 <ahmed-15-5439@diu.edu.bd>
2021-06-25 23:54:00 +0600

    add axios and get data

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const products = require('./data/products');
+const { products } = require('./data/products')
 
 const app = express();
 
@@ -10,8 +10,8 @@ app.get('/api/products',(req,res) => {
     res.json(products)
     })
 app.get('/api/products/:id', (req, res) => {
-    // const product = products.find((p) => p._id === req.params.id)
-    // res.json(product)
-    console.log(req.params.id)
-    })
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product)
+
+})
 app.listen(5000, console.log('server running on port 5000'));
