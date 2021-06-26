b3b7691902895435fc1fad4ef3c8eb7f6a8f946f
monsur22 <ahmed-15-5439@diu.edu.bd>
2021-06-27 00:59:21 +0600

    read single product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import {
 import Home from './components/home/Home';
 import Nav from './components/home/Nav';
 import Footer from './components/home/Footer';
+import Single from './components/home/Single';
 // import AdminHome from './components/admin/AdminHome';
 
 function App() {
@@ -19,6 +20,8 @@ function App() {
     <Nav />
         <Switch>
           <Route  exact  path="/"><Home /></Route>
+          {/* <Route    path="/product"><Single /></Route> */}
+          <Route    path="/product/:id"><Single /></Route>
           {/* <Route    path="/Admin"><AdminHome /></Route> */}
           {/* <Route path="/services"><Services /></Route> */}
 
--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -6,7 +6,6 @@ import {
   Link
 } from "react-router-dom";
 import axios from "axios";
-// import products from "../../products"
 import {Row, Col, Card} from 'react-bootstrap'
 import Nav from './Nav';
 
@@ -25,49 +24,34 @@ const Home = () => {
 			<div class="top-brands">
 				<div class="container">
 					<h3>Hot Offers</h3>
-					<h1>Latest Products</h1>
-					<Row>
-						{products.map(product => (
-							<Col sm={12} md={6} lg={4}>
-								<h3>{product.name}</h3>
-								<Card className='my-3 p-3 rounded'>
-									<a href={'/product/${product._id}'}>
-										<Card.Img src={product.image} variant='top'/>
-									</a>
-								</Card>
-
-							</Col>
-						))}
-					</Row>
 					<div class="agile_top_brands_grids">
-						<div class="col-md-3 top_brand_left">
-							<div class="hover14 column">
-								<div class="agile_top_brand_left_grid">
-									<div class="tag"><img src="../../home/images/tag.png" alt=" " class="img-responsive" /></div>
-									<div class="agile_top_brand_left_grid1">
-										<figure>
-											<div class="snipcart-item block" >
-												<div class="snipcart-thumb">
-													<a href="single.html"><img title=" " alt=" " src="../../home/images/1.png" /></a>
-													<p>fortune sunflower oil</p>
-													<h4>$7.99 <span>$10.00</span></h4>
-												</div>
-												<div class="snipcart-details top_brand_home_details">
-													<form action="checkout.html" method="post">
-														<fieldset>
-															<input type="submit" name="submit" value="Add to cart" class="button" />
-														</fieldset>
-													</form>
+						{products.map(product => (
+							<div class="col-md-3 top_brand_left">
+								<div class="hover14 column">
+									<div class="agile_top_brand_left_grid">
+										<div class="agile_top_brand_left_grid1">
+											<figure>
+												<div class="snipcart-item block" >
+													<div class="snipcart-thumb">
+														<a href={`/product/${product._id}`}><img title=" " alt=" "heigth="140px"width="140px" src={product.image} /></a>
+														<p>{product.name}</p>
+														<h4>{product.price} </h4>
+													</div>
+													<div class="snipcart-details top_brand_home_details">
+														<form action="checkout.html" method="post">
+															<fieldset>
+																<input type="submit" name="submit" value="Add to cart" class="button" />
+															</fieldset>
+														</form>
 
+													</div>
 												</div>
-											</div>
-										</figure>
+											</figure>
+										</div>
 									</div>
 								</div>
 							</div>
-						</div>
-
-
+						))}
 
 						<div class="clearfix"> </div>
 					</div>
