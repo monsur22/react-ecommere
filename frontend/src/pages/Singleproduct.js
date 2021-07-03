3374e887a0274e6d0110447e93bf20f2f51af1c3
monsur22 <ahmed-15-5439@diu.edu.bd>
2021-07-04 00:33:08 +0600

    add redux

--- frontend/src/pages/Singleproduct.js
+++ frontend/src/pages/Singleproduct.js
@@ -3,7 +3,6 @@ import React, {useState ,useEffect} from 'react'
 import axios from 'axios';
 
 const Singleproduct = (props) => {
-        // const product = products.find((x) => x._id === props.match.params.id);
         	const [product, setProduct] = useState([])
 	useEffect(() =>{
 
