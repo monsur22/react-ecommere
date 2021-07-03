41dee85486a740b1f75af3897ea6a2d5e857b2da
monsur22 <ahmed-15-5439@diu.edu.bd>
2021-07-04 01:02:17 +0600

    Redux v_1

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,4 +5,11 @@ import {composeWithDevTools}  from 'redux-devtools-extension'
 
 const reducer = combineReducers({})
 
-const store = createStore(reducer, initialState)
+
+const initialState = {}
+
+const middleware = [thunk]
+
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store
