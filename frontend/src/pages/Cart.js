7bae774bf9c66444b40752aebb04a456e63b13b7
monsur22 <ahmed-15-5439@diu.edu.bd>
2021-07-08 20:38:07 +0600

    Shopping cart v_4

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -13,18 +13,86 @@ const Cart = ({match, location, history}) => {
     const {cartItems} = cart
     console.log(cartItems)
 
-    useEffect(() => {
-        if(productId){
-            dispatch(addToCart(productId, qty))
-        }
-    }, [dispatch, productId, qty])
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty))
+    }
+  }, [dispatch, productId, qty])
     console.log(qty)
 
+    const removeFromCartHandler = (id) =>{
+        console.log('remove')
+    }
+    const checkoutHandler = (id) =>{
+        console.log('checkout')
+    }
 
     return (
-        <div>
-            Cart
-        </div>
+        <div className="cart">
+    <div className="cart-list">
+      <ul className="cart-list-container">
+        <li>
+          <h3>
+            Shopping Cart
+          </h3>
+          <div>
+            Price
+          </div>
+        </li>
+        {
+          cartItems.length === 0 ?
+            <div>
+              Cart is empty
+          </div>
+            :
+            cartItems.map(item =>
+              <li>
+                <div className="cart-image">
+                  <img src={item.image} alt="product" />
+                </div>
+                <div className="cart-name">
+                  <div>
+                    <Link to={"/product/" + item.product}>
+                      {item.name}
+                    </Link>
+
+                  </div>
+                  <div>
+                    Qty:
+                  <select value={item.qty} onChange= {(e) => dispatch(addToCart(item.product, Number(e.target.value)))}>
+                    {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
+                    </select>
+                    <button type="button" className="button" onClick={() => removeFromCartHandler(item.product)} >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+                <div className="cart-price">
+                  ${item.price}
+                </div>
+              </li>
+            )
+        }
+      </ul>
+
+    </div>
+    <div className="cart-action">
+      <h3>
+        Subtotal ( {cartItems.reduce((a, c) => a + c.qty, 0)} items)
+        :
+         $ {cartItems.reduce((a, c) => a + c.price * c.qty, 0).toFixed(2)}
+      </h3>
+      <button onClick={checkoutHandler} className="button primary full-width" disabled={cartItems.length === 0}>
+        Proceed to Checkout
+      </button>
+
+    </div>
+
+  </div>
     )
 }
 
