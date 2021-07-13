b11af211b41b7254f78b93058f12076e3fdf6f06
monsur22 <ahmed-15-5439@diu.edu.bd>
2021-07-13 15:35:34 +0600

    checkout p_1_v_3

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM , CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS} from "../constants/cartConstant"
+import { CART_ADD_ITEM , CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS, CART_SAVE_PAYMENT_METHOD} from "../constants/cartConstant"
 
 export const cartReducer = (state = {cartItems: [], shppingAddress:{}}, action)=>{
     switch(action.type){
@@ -29,6 +29,12 @@ export const cartReducer = (state = {cartItems: [], shppingAddress:{}}, action)=
                 return {
                 ...state,
                 shippingAddress: action.payload,
+                }
+
+            case CART_SAVE_PAYMENT_METHOD:
+                return {
+                ...state,
+                paymentMethod: action.payload,
                 }
 
         default:
