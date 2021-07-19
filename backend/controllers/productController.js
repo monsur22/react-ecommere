a4081203f9f5a86d80a6a8177e946fb06e7485ce
monsur22 <ahmed-15-5439@diu.edu.bd>
2021-07-19 13:16:06 +0600

    Admin P_2_V_2

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -16,4 +16,17 @@ const getProductById = asyncHandler(async(req, res)=>{
 
     }
 })
-export {getProduct, getProductById}
+
+const deleteProduct = asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+      await product.remove()
+      res.json({ message: 'Product removed' })
+    } else {
+      res.status(404)
+      throw new Error('Product not found')
+    }
+  })
+
+export {getProduct, getProductById, deleteProduct}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import {getProduct, getProductById} from '../controllers/productController.js'
+import {getProduct, getProductById, deleteProduct} from '../controllers/productController.js'
+import {protect,admin} from '../middleware/authMeddleware.js'
 
 router.route('/').get(getProduct)
-router.route('/:id').get(getProductById)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
 
 
 
