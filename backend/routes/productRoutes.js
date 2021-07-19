613adadca00747ceb7db234dd613645595921e50
monsur22 <ahmed-15-5439@diu.edu.bd>
2021-07-19 13:32:42 +0600

    Admin P_2_V_3

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import {getProduct, getProductById, deleteProduct} from '../controllers/productController.js'
+import {getProduct, getProductById, deleteProduct, createProduct, updateProduct} from '../controllers/productController.js'
 import {protect,admin} from '../middleware/authMeddleware.js'
 
-router.route('/').get(getProduct)
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
+router.route('/').get(getProduct).post(protect, admin, createProduct)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct)
 
 
 
